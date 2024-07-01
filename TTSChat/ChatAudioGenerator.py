d8703a0948b49c1f81d0daed69199e96160c7d57
laund <laurinschmidt2001@gmail.com>
2024-07-01 22:10:11 +0200

    Update ChatAudioGenerator.py

--- TTSChat/ChatAudioGenerator.py
+++ TTSChat/ChatAudioGenerator.py
@@ -1,27 +1,125 @@
-from TTS.api import TTS
+# requires: RealTimeTTS
+from functools import partial
+from hashlib import md5
+import json
+from operator import mod
 import os
+import queue
 import re
 import socket
-import queue
 import threading
 import time
 import wave
+from pathlib import Path
+from typing import Callable, Dict, List, Optional, Union
+
+from RealtimeTTS import TextToAudioStream, CoquiEngine
+
+
+class TTSPlayer:
+    def __init__(self, model: str) -> None:
+
+        self.engine = CoquiEngine(
+            model_name=model,
+            comma_silence_duration=0.2,
+            sentence_silence_duration=0.3,
+            default_silence_duration=0.1,
+            speed=1.2,
+            voices_path="./samples/",
+        )
+
+        # these lines to find output device index list. select "default output"
+        # import sounddevice as sd
+        # print(sd.query_devices())
+        self.stream = TextToAudioStream(self.engine, output_device_index=8)
+        self.text_filters: List[Callable[[str], Optional[str]]] = []
+        self.cur_voice = ""
+        self.switch_voice("en_sample.wav")
+
+    def add_filter(self, filter: Callable[[str], Optional[str]]):
+        self.text_filters.append(filter)
+
+    def play(self, text: str):
+        for filter in self.text_filters:
+            newtext = filter(text)
+            if newtext is None:
+                return
+            text = newtext
+
+        self.stream.feed(text)
+        self.stream.play()
+
+    def switch_voice(self, voice: Union[str, Path]):
+        if self.cur_voice != str(voice):
+            self.engine.set_voice(str(voice))
+            self.cur_voice = voice
+
+
+class Replacements:
+    def __init__(self) -> None:
+        self.path = Path("replacements.json")
+        self.last_modified = 0
+        self.simple = []
+        self.regex = []
+
+    def check_reload(self):
+        modified = self.path.stat().st_mtime
+        if modified != self.last_modified:
+            with open("replacements.json") as f:
+                content = f.read()
+                data: Dict[str, Dict[str, str]] = json.loads(content)
+                self.simple = [i for i in data["simple"].items()]
+                self.regex = [
+                    (re.compile(rf"\b{r}\b"), w) for r, w in data["words"].items()
+                ] + [(re.compile(r), w) for r, w in data["regex"].items()]
+
+            self.last_modified = modified
+
+    def __call__(self, txt: str) -> str:
+        self.check_reload()
+        for k, v in self.simple:
+            txt = txt.replace(k, v)
+        for rgx, v in self.regex:
+            txt = rgx.sub(v, txt)
+        return txt
+
+
+def bad_msg(txt: str) -> Optional[str]:
+    if re.match(r"^[\W_]+$", txt) or txt.strip() == "":
+        return None
+    return txt
+
 
 class ChatAudioGenerator:
     def __init__(self):
-        os.environ['COQUI_STT_MODEL_CACHE_CONFIRMATION'] = 'true'
         print("Loading TTS model...")
-        self.tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
+        self.tts = TTSPlayer("tts_models/multilingual/multi-dataset/xtts_v2")
+        self.tts.add_filter(bad_msg)
+        self.tts.add_filter(Replacements())
+        self.tts.add_filter(self.voice_filter)
+
         print("TTS model loaded successfully!")
-        self.pathToSamples = "C:/Users/ari/XTTS-v2/samples/"
         self.default_voice = "ariasmoko"
-        self.TWITCH_TOKEN = os.getenv('TWITCH_API_SECRET')
+        self.TWITCH_TOKEN = os.getenv("TWITCH_API_SECRET")
         self.TWITCH_USERNAME = "ariasmoko"
         self.CHANNEL = "#ariasmoko"
         self.message_queue = queue.Queue()
 
+    def voice_filter(self, txt: str) -> Optional[str]:
+        if txt.startswith("!!"):
+            txt = txt[2:]
+            sample, msg = txt.split(" ", maxsplit=1)
+            sample = f"{sample}.wav"
+            samp = Path(self.tts.engine.voices_path) / sample  # type:ignore
+            if samp.exists() and samp.is_file():
+                self.tts.switch_voice(sample)
+                return txt
+            return None
+        self.tts.switch_voice("en_sample.wav")
+        return txt
+
     def connect_to_twitch(self):
-        server = 'irc.chat.twitch.tv'
+        server = "irc.chat.twitch.tv"
         port = 6667
         token = f"oauth:{self.TWITCH_TOKEN}"
         nickname = self.TWITCH_USERNAME
@@ -29,114 +127,61 @@ class ChatAudioGenerator:
 
         sock = socket.socket()
         sock.connect((server, port))
-        sock.send(f"PASS {token}\r\n".encode('utf-8'))
-        sock.send(f"NICK {nickname}\r\n".encode('utf-8'))
-        sock.send(f"JOIN {channel}\r\n".encode('utf-8'))
+        sock.send(f"PASS {token}\r\n".encode())
+        sock.send(f"NICK {nickname}\r\n".encode())
+        sock.send(f"JOIN {channel}\r\n".encode())
 
         return sock
 
-    def listen_for_messages(self, sock):
+    def listen_for_messages(self, sock: socket.socket):
         threading.Thread(target=self.process_messages).start()
         while True:
-            response = sock.recv(2048).decode('utf-8')
+            response = sock.recv(2048).decode("utf-8")
             if self.handlePingResponse(sock, response):
                 continue
             user = self.parse_user(response)
             if user == "nightbot" or user == "Nightbot":
                 continue
-            voice = self.getVoice(user)
             message = self.parse_message(response)
             if message:
-                self.message_queue.put((message, voice))
+                self.message_queue.put(message)
 
     def process_messages(self):
         while True:
-            message, voice = self.message_queue.get()
-            audio_file_path = self.handleMessage(message, voice)
+            message = self.message_queue.get()
+            audio_file_path = self.handle_message(message)
             if audio_file_path:
-                audio_duration = self.get_audio_duration(audio_file_path)
-                time.sleep(audio_duration +.5)
+                return None
 
-    def get_audio_duration(self, file_path):
-        with wave.open(file_path, 'rb') as audio_file:
-            frames = audio_file.getnframes()
-            rate = audio_file.getframerate()
-            duration = frames / float(rate)
-        return duration
-
-    def digit_to_word(self, match):
-        digit_map = {
-            '0': 'zero',
-            '1': 'one',
-            '2': 'two',
-            '3': 'three',
-            '4': 'four',
-            '5': 'five',
-            '6': 'six',
-            '7': 'seven',
-            '8': 'eight',
-            '9': 'nine'
-        }
-        return digit_map[match.group(0)]
-
-    def replace_digits_with_words(self, text):
-        return re.sub(r'\d', self.digit_to_word, text)
-
-    def generate_chat_audio(self, message, voice):
-        if not message:
-            return None
-        return self.generate(message, voice)
-
-    def generate(self, message, voice):
-        file_path = "output.wav"
-        self.tts.tts_to_file(
-            text=message,
-            file_path=file_path,
-            speaker_wav=f"{self.pathToSamples}{voice}.wav",
-            language="en"
-        )
-        return file_path
-
-    def handleMessage(self, message, voice):
+    def handle_message(self, message):
         if message:
-            message = self.replace_digits_with_words(message)
-            message = re.sub(r'\d+', '', message)
-            if len(message) < 15 or voice == "Nightbot":
-                return None
-            message = message[:250]
-            if re.match(r'^[\W_]+$', message) or message.strip() == "":
-                return None
             print(f"Message: {message}")
-            return self.generate_chat_audio(message, voice)
+            return self.tts.play(message)
         return None
 
-    def getVoice(self, user):
-        if user:
-            user_voice_path = f"{self.pathToSamples}{user}.wav"
-            if os.path.exists(user_voice_path):
-                return user
-            else:
-                return "en_sample"
-        return "en_sample"
-
-    def handlePingResponse(self, sock, response):
-        if response.startswith('PING'):
-            sock.send("PONG\n".encode('utf-8'))
+    def handlePingResponse(self, sock: socket.socket, response):
+        if response.startswith("PING"):
+            sock.send(b"PONG\n")
             print("WE PONGED THAT PING!")
             return True
         return False
 
-    def parse_user(self, response):
-        match = re.search(r':(.*?)!.*?@.*?\.tmi\.twitch\.tv PRIVMSG #(.*?) :(.*)', response)
+    def parse_user(self, response) -> Optional[str]:
+        match = re.search(
+            r":(.*?)!.*?@.*?\.tmi\.twitch\.tv PRIVMSG #(.*?) :(.*)", response
+        )
         if match:
             return match.group(1)
         return None
 
     def parse_message(self, response):
-        match = re.search(r':(.*?)!.*?@.*?\.tmi\.twitch\.tv PRIVMSG #(.*?) :(.*)', response)
+        match = re.search(
+            r":(.*?)!.*?@.*?\.tmi\.twitch\.tv PRIVMSG #(.*?) :(.*)", response
+        )
         if match:
             return match.group(3)
         return None
+
 
 if __name__ == "__main__":
     chat_audio_generator = ChatAudioGenerator()
